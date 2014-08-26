e8ed538ad11f805317d72456e9bb1e298a9232df
Rani Itani <itani.rani@gmail.com>
2014-08-26 17:03:12 -0400

    Adding feature to edit pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -20,6 +20,20 @@
     	end 
     end 
 
+    def edit
+    	@picture = Picture.find(params[:id])
+    end 
+
+    def update
+    	@picture = Picture.find(params[:id])
+
+    	if @picture.update_attributes(picture_params)
+    		redirect_to "/pictures/#{@pictures.id}"
+    	else 
+    		render :edit
+    	end 
+    end 
+
     private
 
     def picture_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ get 'pictures/new' => 'pictures#new'
 
 get 'pictures/:id' => 'pictures#show', as: "picture"
 
+get "pictures/:id/edit" => "pictures#edit", as: "edit_picture"
+patch "pictures/:id" => "pictures#update"
+
 # get 'pictures/0' => 'pictures#picture0'
 # get 'pictures/1' => 'pictures#picture1'
 # get 'pictures/2' => 'pictures#picture2'
