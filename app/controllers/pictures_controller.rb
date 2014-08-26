4af8f098c75aaad94e0640ea90d8990eac5547f6
Rani Itani <itani.rani@gmail.com>
2014-08-26 14:25:10 -0400

    making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,22 @@
  class PicturesController < ApplicationController
  	def index
+ 		@pictures = [
+ 			{
+ 				:title => "The old church on the coast of the White sea",
+ 				:artist => "Sergey Eshrov",
+ 				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+ 			},
+ 			{
+ 				:title => "Sea Power",
+ 				:artist => "Stephen Scullion",
+ 				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+ 			},
+ 		    {
+ 				:title => "Into the Poppies",
+ 				:artist => "John Wilhelm",
+ 				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+ 			}
+ 		]
+
  	end 
  end 
