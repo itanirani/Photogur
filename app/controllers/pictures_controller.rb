efa4da12dd0be8b897d6ad47dcc9e140a49ffad8
Rani Itani <itani.rani@gmail.com>
2014-08-26 14:54:24 -0400

    Making each picture show at its own URL using static routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -14,9 +14,33 @@
  		    {
  				:title => "Into the Poppies",
  				:artist => "John Wilhelm",
- 				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+ 				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
  			}
  		]
+    end
 
- 	end 
+ 		def picture0
+ 			@picture = {
+ 				:title => "The old church on the coast of White sea",
+ 				:artist => "Sergey Eshrov",
+ 				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+ 			}
+ 		end 
+
+ 		def picture1
+ 			@picture = {
+ 				:title => "Sea Power",
+ 				:artist => "Stephen Scullion",
+ 				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+ 			}
+ 		end 
+
+ 		def picture2
+ 			@picture = {
+ 				:title => "Into the Poppies",
+ 				:aritst => "John Wilhelm",
+ 				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+ 			}
+ 		end 
+
  end 
