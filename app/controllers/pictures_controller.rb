ed5d5848469e60f569614e793ed067fac86b0f73
Rani Itani <itani.rani@gmail.com>
2014-08-26 15:15:40 -0400

    Refactoring Step #4 to use dynamic routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,28 +19,25 @@
  		]
     end
 
- 		def picture0
- 			@picture = {
- 				:title => "The old church on the coast of White sea",
+ 		def show
+ 		@pictures = [
+ 			{
+ 				:title => "The old church on the coast of the White sea",
  				:artist => "Sergey Eshrov",
  				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
- 			}
- 		end 
-
- 		def picture1
- 			@picture = {
+ 			},
+ 			{
  				:title => "Sea Power",
  				:artist => "Stephen Scullion",
  				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
- 			}
- 		end 
-
- 		def picture2
- 			@picture = {
+ 			},
+ 		    {
  				:title => "Into the Poppies",
- 				:aritst => "John Wilhelm",
+ 				:artist => "John Wilhelm",
  				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
  			}
- 		end 
+ 		]
+ 		@picture = @pictures[params[:id].to_i]
+    end
 
  end 
